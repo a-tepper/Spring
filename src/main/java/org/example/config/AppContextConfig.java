35d431096118f2b411404bd7f529c827e40647bc
Tepper <nbikteeva@gmail.com>
2021-04-06 20:07:27 +0300

    java-based config

---
+++ src/main/java/org/example/config/AppContextConfig.java
@@ -0,0 +1,17 @@
+package org.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+@Configuration
+@ComponentScan(basePackages = "org.example")
+@EnableWebMvc
+public class AppContextConfig implements WebMvcConfigurer {
+}
